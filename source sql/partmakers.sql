af4b842c1bc8afac88950b63aeb4b0657c3f44b3
Jerry Phul <jphul@sonicwall.com>
2012-11-24 22:57:31 -0800

    Update partmaker sql script
    Add code for 'if exists' before attempting to drop table.
    Added commit to end of file to make sure the inserted data gets commited
    to the DB.

--- source sql/partmakers.sql
+++ source sql/partmakers.sql
@@ -1,12 +1,30 @@
-drop table partmakers;
-commit;
+﻿-----
+-- Test and drop table if exists
+BEGIN
+	EXECUTE IMMEDIATE 'drop table partmakers';
 
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+/
+
+
+-----
+-- create the new partmakers table
 CREATE TABLE PARTMAKERS
-(
-name varchar(30) NOT NULL PRIMARY KEY);
+	(name varchar(30) NOT NULL PRIMARY KEY);
 
- -- INSERT INTO PARTMAKERS VALUES ('RADCRX'); 
+
+-----
+-- Add the necessary records to the table
+
+-- INSERT INTO PARTMAKERS VALUES ('RADCRX');
 INSERT INTO PARTMAKERS VALUES ('RDIMARS');
 INSERT INTO PARTMAKERS VALUES ('RDIMBEH');
 INSERT INTO PARTMAKERS VALUES ('RDIMDAN');
 INSERT INTO PARTMAKERS VALUES ('RDIMMOD');
+
+commit;
